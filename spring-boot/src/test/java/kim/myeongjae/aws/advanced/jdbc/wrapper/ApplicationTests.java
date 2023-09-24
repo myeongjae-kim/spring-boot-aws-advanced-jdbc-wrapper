888f3b90a39d928a43a8f06311581bbf40ea1686
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-09-24 16:57:32 +0900

    chore: initialize spring boot project

---
+++ spring-boot/src/test/java/kim/myeongjae/aws/advanced/jdbc/wrapper/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.myeongjae.aws.advanced.jdbc.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
